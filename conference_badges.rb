be149bdf70d205acd9be1e075b4488c3db0144e6
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-17 05:32:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
+people = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    completed_names = []
+    array.each do |name|
+        completed_names.push("Hello, my name is #{name}.")
+    end
+    return completed_names
+end
+
+
+
+def assign_rooms(array)
+    peoples_rooms = []
+    count = 0
+    while count < people.length
+        peoples_rooms.push("Hello, #{people[count]}! You'll be assigned to room #{count + 1}.")
+        count += 1
+    end
+    return peoples_rooms
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
+
+printer(people)
+
 # Write your code here.
